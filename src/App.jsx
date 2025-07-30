4a9e008df1a14b2c222bcd500ce335e204dc4e95
HROON157 <haroonshafiq104@gmail.com>
2025-07-31 02:16:38 +0500

    Color Change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <p className="text-2xl text-center py-4 font-bold text-blue-500">This is the App of connect </p>
+      <p className="text-2xl text-center py-4 font-bold text-blue-600">This is the App of connect </p>
     </>
   )
 }
