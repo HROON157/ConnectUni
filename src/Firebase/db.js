10693c1cfa15b88810e94a5dcb43051c17a67744
HROON157 <haroonshafiq104@gmail.com>
2025-08-20 21:25:07 +0500

    Final code pushed with db.js

--- src/Firebase/db.js
+++ src/Firebase/db.js
@@ -6,6 +6,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
