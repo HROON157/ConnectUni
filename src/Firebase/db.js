1fafd65ab1d849b50686569f7bc36ff79dfe4e23
HROON157 <haroonshafiq104@gmail.com>
2025-08-20 21:18:13 +0500

    Final code pushed with db.js

--- src/Firebase/db.js
+++ src/Firebase/db.js
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVTWtFf3Lo2dWxPJoUN8Y5gRcN-fL9Hkc",
-  authDomain: "connectuni-e0a42.firebaseapp.com",
-  projectId: "connectuni-e0a42",
-  storageBucket: "connectuni-e0a42.firebasestorage.app",
-  messagingSenderId: "1017457439064",
-  appId: "1:1017457439064:web:24b29c86ea57424e4fb52d",
-  measurementId: "G-7R59V85WDH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
